0371b017ac415626b75b6b45a085b11622d838a6
__Saritah__ <142619907+Saramarinho@users.noreply.github.com>
2023-08-31 17:27:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+/*sara this is a js file
+A.Exercícios de interpretação de código!
+1.console.log(minhaFunção(2)); //10
+console.log(minhafunção(10)); //50
+'invalid or unexpected token';
+
+2.a) //Essa funçaõ serve para alterar o texto digitado pelo usuário, primeiramente se usa uma variável
+pedindo para o usuário digitar um texto, logo depois ele cria uma outra variável com uma função com um 
+parâmetro texto, depois é pedido para retornar o texto em letras minúsculas e responder se a palavra "cenoura",
+está junto da frase digitada pelo usuário,
+depois se cria uma outra variável chamada resposta, que seria o resultado das alterações, junto com o 
+texto digitado pelo usuário, depois é só dar um console.log da variável (resposta).
+E ela irá imprimir o dígito do usuário com as alterações feitas!
+
+//eu gosto de cenoura (true)
+cenoura é bom pra vista (true)
+cenouras crescem na terra (false)
+
+B.Exercícios de escrita de código!
+1.a)
+
+function information() {
+    const myInformation = ("Eu sou Sara, tenho 16 anos, moro em São Leopoldo e sou estudante.")
+    console.log(myInformation);
+}
+    information()
+
+   b) 
+    function informação(n, i, c, p) {
+    return "Eu sou " + n + ", tenho " +  +
+
+
+    2. function somaNumeros(a, b){
+        return a + b;
+    }
+
+
+*/
+function calculoSoma(a, b) {
+    return a + b;
+    
+}
+
+
+function calculoSubtração(a, b) {
+    return a - b;
+    
+}
+
+
+function calculoMulti(a, b) {
+    return a * b;
+    
+}
+
+    
+function calculoDiv(a, b) {
+    return a / b;
+
+}
+
+let n1 = Number(prompt("Digite o primeiro numero"));
+let n2 = Number(prompt("Digite o segundo numero"));
+
+console.log()
+
+
+        Não terminei ;-;
+
+ 
